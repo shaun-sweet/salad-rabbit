2050e3dc718b9037e7249aa8cab72c2c7a8a4503
shaun-sweet <shaun@shaunsweet.com>
2017-02-20 12:19:36 -1000

    didn't have package installed to test out

--- src/App.js
+++ src/App.js
@@ -1,60 +1,26 @@
 import React, { Component } from 'react';
 import './css/App.css';
-
-
-var Dropbox = require('dropbox');
-var dbx = new Dropbox({ accessToken: 'DDIAux8ajggAAAAAAAAIFMQMfRsFSEa5KdP45To_6wW5iFJrtQfNtkeXwTxvzRce' });
-// var reader = new FileReader();
-// reader.addEventListener("loadend", function() {
-//    // reader.result contains the contents of blob as a typed array
-//    console.log(JSON.parse(reader.result));
-// });
-//
-// dbx.filesDownload({path: '/file.salad'})
-//   .then(function(response) {
-//     console.log(response);
-//     reader.readAsText(response.fileBlob)
-//   })
-//   .catch(function(error) {
-//     console.log(error);
-//   });
-// dbx.filesListFolder({path: ''})
-//   .then(function(response) {
-//     console.log(response);
-//   })
-//   .catch(function(error) {
-//     console.log(error);
-//   });
-dbx.filesUpload({
-  contents: JSON.stringify({helloWorld: "vivian likes auto saving features coupled with hot reloading web servers"}),
-  path: "/file.salad",
-  mode: {".tag": "overwrite"},
-  autorename: false,
-  mute: false,
-
-})
-  .then((response) => console.log(response))
-  .catch((error) => console.error(error));
-import seed from './seed';;
-
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import './helpers';
+injectTapEventPlugin();
 class App extends Component {
 
   constructor() {
     super();
-    this.state = seed;
+    this.state = {
+
+      };
 
   }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider iconClassNameRight="muidocs-icon-navigation-expand-more">
+        <AppBar title="Salad Rabbit" />
+      </MuiThemeProvider>
     );
   }
 }
