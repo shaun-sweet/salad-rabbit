73f86e2a260349bf70c5ac3df706b7494a625a91
shaun-sweet <shaun@shaunsweet.com>
2017-02-21 15:29:45 -0700

    changed index.js to renderToDom to avoid confusion with index in other folders

--- src/store.js
+++ src/store.js
@@ -6,50 +6,51 @@ const middleware = applyMiddleware(thunk, logger())
 export default createStore(reducers, middleware);
 
 // is just to see the data model, it really does nothing
-// {
-//   closed_accounts: [
-//     {
-//       name: "Checking",
-//       type: "Checking",
-//       balance: 0,
-//       note: "String"
-//     }
-//   ],
-//   accounts: [
-//     {
-//       name: "Checking",
-//       type: "checking",
-//       balance: 100000,
-//       note: "String"
-//     },
-//     {
-//       name: "Credit Card",
-//       type: "credit card",
-//       balance: -100,
-//       note: "String"
-//     }
-//   ],
-//   transactions: [
-//     {
-//       account: "Checking",
-//       date: Date.now,
-//       payee: "Dominos",
-//       category: "Food",
-//       memo: "twas good",
-//       outflow: 30.05,
-//       inflow: null,
-//       cleared: true
-//     }
-//   ],
-//   categories: [
-//     {
-//       master_category: "Monthly Bills",
-//       sub_categories: [
-//         {
-//           name: "Rent",
-//           budgeted_amount: 500
-//         }
-//       ]
-//     }
-//   ]
-// }
+// eslint-disable-next-line
+var s = {
+  closed_accounts: [
+    {
+      name: "Checking",
+      type: "Checking",
+      balance: 0,
+      note: "String"
+    }
+  ],
+  accounts: [
+    {
+      name: "Checking",
+      type: "checking",
+      balance: 100000,
+      note: "String"
+    },
+    {
+      name: "Credit Card",
+      type: "credit card",
+      balance: -100,
+      note: "String"
+    }
+  ],
+  transactions: [
+    {
+      account: "Checking",
+      date: Date.now,
+      payee: "Dominos",
+      category: "Food",
+      memo: "twas good",
+      outflow: 30.05,
+      inflow: null,
+      cleared: true
+    }
+  ],
+  categories: [
+    {
+      master_category: "Monthly Bills",
+      sub_categories: [
+        {
+          name: "Rent",
+          budgeted_amount: 500
+        }
+      ]
+    }
+  ]
+}
