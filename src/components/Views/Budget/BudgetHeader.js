7148b9279e23582aed2d68f8a4b5547e52d0556b
Vivian Lo <vivianlo789@gmail.com>
2017-02-23 16:10:53 -0800

    fix arithmetic for changing budgeted amount

--- src/components/Views/Budget/BudgetHeader.js
+++ src/components/Views/Budget/BudgetHeader.js
@@ -6,9 +6,9 @@ export default class BudgetHeader extends Component {
     //total all available funds
     const accounts = this.props.accounts.reduce((accumulator, element)=> accumulator + parseInt(element.balance), 0);
     //total all budgeted columns
-    const budgeted = this.props.categories.reduce((accumulator, element)=> accumulator + element.subcategories.reduce((acc, elem)=>acc + elem.budgeted, 0), 0);
+    const budgeted = this.props.categories.reduce((accumulator, element)=> accumulator + element.subcategories.reduce((acc, elem)=>acc + parseInt(elem.budgeted), 0), 0);
     //total all outflows columns
-    const outflows = this.props.categories.reduce((accumulator, element)=> accumulator + element.subcategories.reduce((acc, elem)=>acc + elem.outflow, 0), 0);
+    const outflows = this.props.categories.reduce((accumulator, element)=> accumulator + element.subcategories.reduce((acc, elem)=>acc + parseInt(elem.outflow), 0), 0);
     return (
       <div id="budget">
         <div id="available-to-budget">
