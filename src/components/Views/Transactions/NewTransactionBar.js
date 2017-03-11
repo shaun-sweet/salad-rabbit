118ee9daffb75ab34f94e53675c56dd64059d791
shaun <shaun@shaunsweet.com>
2017-03-10 23:16:10 -0700

    refactored new transaction bar into stateless componenents

--- src/components/Views/Transactions/NewTransactionBar.js
+++ src/components/Views/Transactions/NewTransactionBar.js
@@ -3,94 +3,89 @@ import RaisedButton from 'material-ui/RaisedButton'
 import DatePicker from 'react-datepicker'
 require('react-datepicker/dist/react-datepicker.css');
 
-export default class NewTransactionBar extends Component {
-  render() {
-    const inputProps = (key) => ({
-      onChange: this.props.onChange,
-      name: key,
-      placeholder: key,
-      type: 'text',
-      className: 'new-transaction'
-    });
-    let clearedProps = {
-      ...inputProps('cleared'),
-      onChange: this.props.onCheckboxChange,
-      type: 'checkbox',
-      defaultChecked: true
-    };
-    return (
-      <div className='new-transaction-bar'>
-        <div className="row">
-          <div>
-            <AccountDropdown
-              onChange={this.props.onChange}
-              accounts={this.props.accounts}
-            />
-          </div>
-          <div>
-            <DatePicker
-              id="date-picker"
-              todayButton="Today"
-              onChange={this.props.onDateChange}
-              selected={this.props.selectedDate}
-            />
-          </div>
-          <div>
-            <input {...inputProps('payee')} />
-          </div>
-          <div>
-            <CategoryDropdown
-              onChange={this.props.onChange}
-              categories={this.props.masterCategories}
-            />
-          </div>
-          <div>
-            <input {...inputProps('memo')} />
-          </div>
-          <div>
-            <input {...inputProps('outflow')} />
-          </div>
-          <div>
-            <input {...inputProps('inflow')} />
-          </div>
-          <div>
-            <label>Cleared</label>
-          </div>
-          <div>
-            <input {...clearedProps} />
-          </div>
+const NewTransactionBar = (props) => {
+  const inputProps = (key) => ({
+    onChange: props.onChange,
+    name: key,
+    placeholder: key,
+    type: 'text',
+    className: 'new-transaction'
+  });
+  let clearedProps = {
+    ...inputProps('cleared'),
+    onChange: props.onCheckboxChange,
+    type: 'checkbox',
+    defaultChecked: true
+  };
+  return (
+    <div className='new-transaction-bar'>
+      <div className="row">
+        <AccountDropdown
+          onChange={props.onChange}
+          accounts={props.accounts}
+        />
+        <div className='new-transaction'>
+          <DatePicker
+            id="date-picker"
+            todayButton="Today"
+            onChange={props.onDateChange}
+            selected={props.selectedDate}
+          />
         </div>
-        <RaisedButton onClick={this.props.handleSaveNewTransaction} className="save-transaction-button" label="Save" />
-        <RaisedButton onClick={this.props.handleCancelTransaction} className="cancel-transfer-button" label="Cancel" />
+        <FormInput {...inputProps('payee')} />
+        <CategoryDropdown
+          onChange={props.onChange}
+          categories={props.masterCategories}
+        />
+        <FormInput {...inputProps('memo')} />
+        <FormInput {...inputProps('outflow')} />
+        <FormInput {...inputProps('inflow')} />
+        <FormInput {...clearedProps} />
       </div>
-    );
-  }
-
+      <RaisedButton onClick={props.handleSaveNewTransaction} className="save-transaction-button" label="Save" />
+      <RaisedButton onClick={props.handleCancelTransaction} className="cancel-transfer-button" label="Cancel" />
+    </div>
+  )
 }
 
+const FormInput = (props) => (
+  <div>
+    <input {...props} />
+  </div>
+)
 
+FormInput.defaultProps = {
+  type: 'text',
+  className: 'new-transaction'
+}
 
 const AccountDropdown = (props) => (
-  <select onChange={props.onChange} name='account'>
-    {props.accounts.map((account) => {
-      return <option key={account.id} value={account.id}>{account.name}</option>
-    })}
-  </select>
+  <div className={props.className}>
+    <select onChange={props.onChange} name='account'>
+      {props.accounts.map((account) => {
+        return <option key={account.id} value={account.id}>{account.name}</option>
+      })}
+    </select>
+  </div>
 )
 
 
 const CategoryDropdown = (props) => (
-  <select onChange={props.onChange} name='category'>
-    {props.categories.map((masterCategory) => {
-      return (
-        <optgroup key={masterCategory.id} label={masterCategory.name}>
-          {masterCategory.categories.map((category) => {
-            return (
-              <option key={category.id} value={category.id}>{category.name}</option>
-            );
-          })}
-        </optgroup>
-      );
-    })}
-  </select>
+  <div className={props.className}>
+    <select onChange={props.onChange} name='category'>
+      {props.categories.map((masterCategory) => {
+        return (
+          <optgroup key={masterCategory.id} label={masterCategory.name}>
+            {masterCategory.categories.map((category) => {
+              return (
+                <option key={category.id} value={category.id}>{category.name}</option>
+              );
+            })}
+          </optgroup>
+        );
+      })}
+    </select>
+  </div>
 )
+
+module.exports = NewTransactionBar;
