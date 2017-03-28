5cd206217f30f81c8c2a9c3db7a1ce27f8b29472
Vivian Lo <vivian@Vivians-MacBook-Pro.local>
2017-03-28 00:38:15 -0700

    update dropbox-test to use fileDownload method

--- src/dropbox-test.js
+++ src/dropbox-test.js
@@ -19,36 +19,13 @@ dbx.filesUpload({path: '/salad-rabbit', contents: JSON.stringify({"one": "1", "t
     console.error(error);
   });
 
-
-var downloaded_file;
-//link where you want to download from.
-var link = 'https://www.dropbox.com/s/mrwbq89cydtwl9q/salad-rabbit?dl=0';
-
-//if there is no link created yet, you want to run this to create the link
-if(!link){
-  dbx.sharingCreateSharedLinkWithSettings({path: '/salad-rabbit'})
-    .then(function(response) {
-      //use the created shared link to download the file
-      link = response.url;
-      downloadFile(link);
-    })
-    .catch(function(error) {
-      console.error(error);
-    });
-}else{
-  downloadFile(link);
-}
-
-function downloadFile(url){
-  dbx.sharingGetSharedLinkFile({url: url})
-    .then(function(response){
-      json_downloaded = JSON.parse(response.fileBinary);
-      console.log(json_downloaded);
-      console.log(json_downloaded["one"]);
-      downloaded_file = json_downloaded;
-      console.log(downloaded_file);
-    })
-    .catch(function(error){
-      console.error(error)
-    })
-}
+var downloadedJSON;
+dbx.filesDownload({path: '/salad-rabbit'})
+  .then(function(response){
+    downloadedJSON = JSON.parse(response.fileBinary);
+    console.log(downloadedJSON);
+    console.log(downloadedJSON.one);
+  })
+  .catch(function(error){
+    console.error(error);
+  })
