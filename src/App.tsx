1ac99adec095a09a39e71237142f6e70114f1c95
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-19 00:56:51 +0000

    Deploy shaun-sweet/salad-rabbit to github.com/shaun-sweet/salad-rabbit.git:gh-pages

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
