8faa563cc6c89b9765f5bfd0a580951ca1578418
shaun <shaun.sweet@carvana.com>
2019-07-18 17:52:24 -0700

    Updates

---
+++ precache-manifest.6ebf59ce9e270f281720d68b482a8b99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a7c0f7475bde0bb1e586c12f05a9c12",
+    "url": "/salad-rabbit/index.html"
+  },
+  {
+    "revision": "071396fb240e1968ad54",
+    "url": "/salad-rabbit/static/css/2.7c06dbe9.chunk.css"
+  },
+  {
+    "revision": "071396fb240e1968ad54",
+    "url": "/salad-rabbit/static/js/2.97f4a80b.chunk.js"
+  },
+  {
+    "revision": "2ab31396fc45797bc098",
+    "url": "/salad-rabbit/static/js/main.a4ed7b42.chunk.js"
+  },
+  {
+    "revision": "21c4a7eebaa92f91d78b",
+    "url": "/salad-rabbit/static/js/runtime~main.7419e475.js"
+  }
+]);
