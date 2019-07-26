fd0b2c3224d8487d2c4a949a163e310c5f133ce6
shaun sweet <shaun@shaunsweet.com>
2019-07-26 00:44:17 -0700

    updating auth url stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Button } from 'antd';
+import { Dropbox } from 'dropbox';
 
+const dbx = new Dropbox({ clientId: 'kl4fz9vabg5el9q', fetch: fetch });
+
+const authUrl = dbx.getAuthenticationUrl('http://localhost:3000');
 const App: React.FC = () => {
   return (
     <div className="App">
+    <a href={authUrl}>
       <Button type="primary">Button</Button>
+    </a>
     </div>
   );
 }
