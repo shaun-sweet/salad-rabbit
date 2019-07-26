b3d1295e3bb3f1c62503efa7d1106dd81d736ef1
Shaun Sweet <shaun@shaunsweet.com>
2019-07-26 00:54:14 -0700

    more testing

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ const dbx = new Dropbox({ clientId: 'kl4fz9vabg5el9q', fetch: fetch });
 
 const authUrl = dbx.getAuthenticationUrl('http://localhost:3000');
 const App: React.FC = () => {
-  console.log(process.env)
+  console.log(process.env);
+  
   return (
     <div className="App">
     <a href={authUrl}>
